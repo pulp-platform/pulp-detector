b666b0a456737a52e08dd9a7cde8609343c86950
Lorenzo Lamberti <lorenzo.lamberti@unibo.it>
2023-06-27 10:25:41 +0200

    updated copyright

--- crazyflie_app/random-following-spiral/inc/config_main.h
+++ crazyflie_app/random-following-spiral/inc/config_main.h
@@ -1,25 +1,28 @@
 /*-----------------------------------------------------------------------------
- Copyright (C) 2023 University of Bologna, Italy, ETH Zurich, Switzerland. 
+ Copyright (C) 2023 University of Bologna, Italy.
  All rights reserved.                                                           
-                                                                               
+
  Licensed under the Apache License, Version 2.0 (the "License");               
  you may not use this file except in compliance with the License.              
  See LICENSE.apache.md in the top directory for details.                       
  You may obtain a copy of the License at                                       
-                                                                               
+
    http://www.apache.org/licenses/LICENSE-2.0                                  
-                                                                               
+
  Unless required by applicable law or agreed to in writing, software           
  distributed under the License is distributed on an "AS IS" BASIS,             
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.      
  See the License for the specific language governing permissions and           
- limitations under the License.                                
+ limitations under the License.                                                
 
- File:    config_main.h                                                              
- Author:  Lorenzo Lamberti      <lorenzo.lamberti@unibo.it>                           
- Date:    18.02.2022                                                           
+ File:   	config_main.c
+ Authors:
+	  		Lorenzo Lamberti 	<lorenzo.lamberti@unibo.it>
+         	Luca Bompani  		<luca.bompani5@unibo.it>
+			Manuele Rusci 		<manuele.rusci@kuleuven.be>
+			Daniele Palossi 	<dpalossi@ethz.ch> <daniele.palossi@supsi.ch>                   
+ Date:   	01.04.2023                                                          
 -------------------------------------------------------------------------------*/
-
 
 // Flight 
 #define FORWARD_VELOCITY          0.10f   // Max forward speed [m/s].  Default: 1.0f
--- crazyflie_app/rotate/src/rotate.c
+++ crazyflie_app/rotate/src/rotate.c
@@ -1,20 +1,37 @@
 /*-----------------------------------------------------------------------------
- Copyright (C) 2023 University of Bologna, Italy. 
+ Copyright (C) 2023 University of Bologna, Italy.
  All rights reserved.                                                           
-                                                                               
-Thesis based on autonomous driving of drones able to map an unknown
-room autonomously through the sensors of the multiranger deck of the
-crazyflie 2.0 drone;
-rotation mode: the drone performs a 360 ° rotation on itself and in the
+
+ Licensed under the Apache License, Version 2.0 (the "License");               
+ you may not use this file except in compliance with the License.              
+ See LICENSE.apache.md in the top directory for details.                       
+ You may obtain a copy of the License at                                       
+
+   http://www.apache.org/licenses/LICENSE-2.0                                  
+
+ Unless required by applicable law or agreed to in writing, software           
+ distributed under the License is distributed on an "AS IS" BASIS,             
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.      
+ See the License for the specific language governing permissions and           
+ limitations under the License.                                                
+
+ File:   	rotate.c
+ Authors:
+	  		Lorenzo Lamberti 	<lorenzo.lamberti@unibo.it>
+ 		  	Davide Graziani      <davide.graziani4@studio.unibo.it>
+         	Luca Bompani  		<luca.bompani5@unibo.it>
+			Manuele Rusci 		<manuele.rusci@kuleuven.be>
+			Daniele Palossi 	<dpalossi@ethz.ch> <daniele.palossi@supsi.ch>
+
+ Date:   	01.04.2023                                                          
+-------------------------------------------------------------------------------*/
+
+/* Description:                                                       
+rotation policy: the drone performs a 360 ° rotation on itself and in the
 meantime acquires eight distance measurements which it will then use
 to determine the maximum distance; at this point it moves by a
 predetermined distance in that direction.
-                                                                               
- File:    rotate.c         
- Author:  Lorenzo Lamberti      <lorenzo.lamberti@unibo.it>                                             
- 		  Davide Graziani      <davide.graziani4@studio.unibo.it>
- Date:    08.04.2022                                                           
--------------------------------------------------------------------------------*/
+*/
 
 #include <string.h>
 #include <stdint.h>
